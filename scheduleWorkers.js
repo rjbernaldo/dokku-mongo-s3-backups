a8e6506af172f3a27a645f21adfdbbb2f731aec5
Rj Bernaldo <rj.bernaldo@gmail.com>
2018-05-09 17:32:50 +1000

    Fix scheduler to run every 1st of the month

--- scheduleWorkers.js
+++ scheduleWorkers.js
@@ -2,7 +2,6 @@ const schedule = require('node-schedule');
 
 const backup = require('./workers/backup');
 
-// schedule.scheduleJob('0 0 * * *', () => {
-//   backup();
-// });
+schedule.scheduleJob('0 0 1 * *', () => {
   backup();
+});
