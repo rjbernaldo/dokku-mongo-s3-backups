52e286c8a9b85b245f64e00e8d95c5e66508e556
Rj Bernaldo <rj.bernaldo@gmail.com>
2018-05-09 18:07:14 +1000

    Add log

--- scheduleWorkers.js
+++ scheduleWorkers.js
@@ -3,5 +3,6 @@ const schedule = require('node-schedule');
 const backup = require('./workers/backup');
 
 schedule.scheduleJob('0 0 1 * *', () => {
+  console.log('Backing up db');
   backup();
 });
