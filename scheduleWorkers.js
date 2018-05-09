88c786b20085bc15c599ea4ab3c53d6cc6db5814
Rj Bernaldo <rj.bernaldo@gmail.com>
2018-05-09 17:35:55 +1000

    Add small note when scheduler runs

--- scheduleWorkers.js
+++ scheduleWorkers.js
@@ -3,5 +3,6 @@ const schedule = require('node-schedule');
 const backup = require('./workers/backup');
 
 schedule.scheduleJob('0 0 1 * *', () => {
+  console.log('Scheduled job to run every 1st of the month');
   backup();
 });
